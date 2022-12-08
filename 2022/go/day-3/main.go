eb0bd91934f4cfecea85427a55760f7e33913dff
Adrephos <adrephos@gmail.com>
2022-12-08 09:53:01 -0500

    feat: finish day 3 puzzle 2

--- 2022/go/day-3/main.go
+++ 2022/go/day-3/main.go
@@ -6,37 +6,71 @@ import (
 	"strings"
 )
 
-func main() {
-	f, _ := ioutil.ReadFile("input.txt")
-
-	file := string(f)
-
-	rucksack := strings.Split(file, "\n")
-
+func findWrongPlaced(rucksack []string) int {
 	total := 0
 
 	for _, items := range rucksack {
 		if len(items) != 0 {
 			half := (len(items) / 2)
-			compartment1 := items[0:half]
-			compartment2 := items[half:]
-			priority := 0
-			for _, item1 := range compartment1 {
-				for _, item2 := range compartment2 {
-					if item1 == item2 {
-						ascii := int(item1)
-						if ascii >= 65 && ascii <= 90 {
-							priority = ascii - 38
-						} else {
-							priority = ascii - 96
-						}
-					}
-				}
-			}
+			compartment1, compartment2 := items[0:half], items[half:]
 
+			priority := 0
+
+			index := strings.IndexAny(compartment1, compartment2)
+			ascii := int(compartment1[index])
+
+			if ascii >= 65 && ascii <= 90 {
+				priority = ascii - 38
+			} else {
+				priority = ascii - 96
+			}
 			total += priority
 		}
 
 	}
-	fmt.Println(total)
+	return total
+}
+
+func getCommon(str, str2 string) string {
+	common := ""
+	for _, rune1 := range str {
+		for _, rune2 := range str2 {
+			if rune1 == rune2 {
+				common += string(rune1)
+			}
+		}
+	}
+	return common
+}
+
+func findBadges(rucksack []string) int {
+	lngt := len(rucksack)
+	total := 0
+	for i := 0; i <= lngt-3; i += 3 {
+		elf1, elf2, elf3 := rucksack[i], rucksack[i+1], rucksack[i+2]
+
+		common := getCommon(elf1, elf2)
+		index := strings.IndexAny(common, elf3)
+		priority := 0
+		ascii := int(common[index])
+
+		if ascii >= 65 && ascii <= 90 {
+			priority = ascii - 38
+		} else {
+			priority = ascii - 96
+		}
+		total += priority
+
+	}
+
+	return total
+}
+
+func main() {
+	f, _ := ioutil.ReadFile("input.txt")
+	file := string(f)
+	rucksack := strings.Split(file, "\n")
+
+	fmt.Println("Puzzle 1:", findWrongPlaced(rucksack))
+	fmt.Println("Puzzle 2:", findBadges(rucksack))
 }
