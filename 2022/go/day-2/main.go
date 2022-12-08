0b4018c620749db248a0f590be9be76af9dbd71c
Adrephos <adrephos@gmail.com>
2022-12-07 19:13:01 -0500

    feat: finish day 2 puzzle 2

--- 2022/go/day-2/main.go
+++ 2022/go/day-2/main.go
@@ -31,11 +31,37 @@ func puzzle1(file string) int {
 
 }
 
+func puzzle2(file string) int {
+	total := 0
+
+	posibilities := map[string]int{
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
+	}
+
+	rounds := strings.Split(file, "\n")
+
+	for _, round := range rounds {
+		total += posibilities[round]
+	}
+
+	return total
+
+}
+
 func main() {
 	f, _ := ioutil.ReadFile("input.txt")
 
 	file := string(f)
 
 	fmt.Println("Puzzle 1:", puzzle1(file))
+	fmt.Println("Puzzle 2:", puzzle2(file))
 
 }
